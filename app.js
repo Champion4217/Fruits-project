42a6bbace9e7e1622c97d035447f3be84e73a65f
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-03-21 23:51:34 +0530

    on the way

---
+++ app.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
+const insertDocuments = function(db,callback){
+    const collection = db.collection("fruits");
+    collection.insertMany([
+        {
+            name : "Apple",
+            score : 8,
+            review: "Great fruit"
+        },
+        {
+            name : "Orange",
+            score: 6,
+            review: "kinda sour"
+        },
+        {
+            name : "Mango",
+            score : 10,
+            review : "Mindblowing"
+        }    
+
+    ], function(err,result){
+        assert.equal(err,null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("inserted 3 documents");
+        callback(result);
+    });
+};
+
+const findDocuments = function(db, callback){
+    const collection = db.collection("fruits");
+    collection.find({}).toArray(function(err, fruits){
+        assert.equal(err,null);
+        console.log("found");
+        console.log(fruits)
+        callback(fruits);
+    });
+};
